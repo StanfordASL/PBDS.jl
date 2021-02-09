c451f0a72d110c6cff78a12c8a39f813e2a776a4
ambyld <bylard@stanford.edu>
2021-02-08 22:46:34 -0800

    Add arm example test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,8 @@ using Test
 using NBInclude
 
 @testset "Examples" begin
+    dir = joinpath(@__DIR__, "..", "examples", "PBDS")
+
     test_notebooks = ["R1_To_R1PointPositionAttractor",
                       "R2_To_R1PointDistanceAttractor_S1Damping",
                       "R2_To_R1PointDistanceAttractor_S1Damping_R1BoxAvoidance",
@@ -14,11 +16,18 @@ using NBInclude
 
     for test_notebook in test_notebooks
         @testset "$test_notebook" begin
-            dir = joinpath(@__DIR__, "..", "examples", "PBDS")
             file = string(test_notebook, ".ipynb")
             @nbinclude(joinpath(dir, file))
             @test all([norm(traj.xm[end] - xm_goal) for traj in trajs] .< 5e-2)
+            println("Finished example ", test_notebook)
         end
     end
 
+    test_notebook = "R7Arm_DynamicMugGrasping"
+    @testset "$test_notebook" begin
+        file = string(test_notebook, ".ipynb")
+        @nbinclude(joinpath(dir, file))
+        @test root.children[end-1].children[3].traj_log.x[end][1] .< 5e-3
+        println("Finished example ", test_notebook)
+    end
 end
