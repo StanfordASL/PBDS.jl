8d9d77ed54110f76379755d90c926dfa689e5d8d
ambyld <bylard@stanford.edu>
2021-02-10 12:15:21 -0800

    Add tests for RMPflow, task list trajectory logs, robot chart coord rep

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,12 +4,16 @@ using NBInclude
 
 @testset "Examples" begin
     "no_plots" in ARGS && (global const no_plots = true)
-    dir = joinpath(@__DIR__, "..", "examples", "PBDS")
+    PBDS_dir = joinpath(@__DIR__, "..", "examples", "PBDS")
 
     test_notebook = "R7Arm_DynamicMugGrasping"
     @testset "$test_notebook" begin
         file = string(test_notebook, ".ipynb")
-        @nbinclude(joinpath(dir, file))
+        @nbinclude(joinpath(PBDS_dir, file))
+        @test root.children[end-1].children[3].traj_log.x[end][1] .< 5e-3
+        robot_coord_rep = ChartRep()
+        traj = propagate_tasks(xm, vm, root, CM, Time, dt, robot_coord_rep, state, cache, 
+            mugparams; time_dep, log_tasks=true)
         @test root.children[end-1].children[3].traj_log.x[end][1] .< 5e-3
         println("Finished example ", test_notebook)
     end
@@ -23,12 +27,53 @@ using NBInclude
                       "R3_To_R1PointDistanceAttractor_S2Damping_R1SphereAvoidance",
                       "S2_To_R1Attractor_S2Damping_R1ObstacleAvoidance"]
 
+    test_notebooks = ["R3_To_R1PointDistanceAttractor_S2Damping_R1SphereAvoidance"]
+
     for test_notebook in test_notebooks
         @testset "$test_notebook" begin
             file = string(test_notebook, ".ipynb")
-            @nbinclude(joinpath(dir, file))
-            @test all([norm(traj.xm[end] - xm_goal) for traj in trajs] .< 5e-2)
+            @nbinclude(joinpath(PBDS_dir, file))
+            ε = 5e-2
+            @test all([norm(traj.xm[end] - xm_goal) for traj in trajs] .< ε)
+
+            if test_notebook == "R3_To_R1PointDistanceAttractor_S2Damping_R1SphereAvoidance"
+                log_tasks = true
+                robot_coord_rep = ChartRep()
+                traj = propagate_tasks(xm, vm, tasks, CM, CNs, Time, dt, robot_coord_rep; log_tasks)
+                @test norm(traj.xm[end] - xm_goal) < ε
+                robot_coord_rep = EmbRep()
+                traj = propagate_tasks(xm, vm, tasks, CM, CNs, Time, dt, robot_coord_rep; log_tasks)
+                @test norm(traj.xm[end] - xm_goal) < ε
+            end
             println("Finished example ", test_notebook)
         end
     end
+
+    test_notebook = "S2_To_R1Attractor_S2Damping_ConsistencyTest"
+    @testset "$test_notebook" begin
+        file = string(test_notebook, ".ipynb")
+        @nbinclude(joinpath(PBDS_dir, file))
+        Δx = 0.
+        Δx += sum(@. norm(traj_north.xm - traj_south.xm))
+        Δx += sum(@. norm(traj_south.xm - traj_switching.xm))
+        Δx += sum(@. norm(traj_switching.xm - traj_north.xm))
+        @test Δx < 1e-4
+        ε = 5e-3
+        @test norm(traj_north.xm[end] - xm_goal) < ε
+
+        RMP_dir = joinpath(@__DIR__, "..", "examples", "RMPflow")
+        @nbinclude(joinpath(RMP_dir, file))
+        Δx = 0.
+        Δx += sum(@. norm(traj_north.xm - traj_south.xm))
+        Δx += sum(@. norm(traj_south.xm - traj_switching.xm))
+        Δx += sum(@. norm(traj_switching.xm - traj_north.xm))
+        @test Δx > 1e3
+        Δx = 0.
+        Δx += norm(traj_north.xm[end] - xm_goal)
+        Δx += norm(traj_south.xm[end] - xm_goal)
+        Δx += norm(traj_switching.xm[end] - xm_goal)
+        @test Δx < ε
+        println("Finished example ", test_notebook)
+    end
+
 end
